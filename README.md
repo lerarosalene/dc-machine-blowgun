6e08b2b4f179432aeeb258386979db4bd488aeac
Larissa Rosalene <lerarosalene@outlook.com>
2023-03-19 21:35:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Dead Cells Mod: Machine Blowgun
+
+This mod:
+- changes blowgun rate of fire to ~50 per second
+- changes its base poison damage from 15/sec for 2 seconds to 15000/sec for 2000 seconds
+- gives you free blowgun at start of the game (after first 2 tutorial runs)
+
+With this changes blowgun without upgrades and 1-1-1 stats can melt anything up to final 5BC boss.
+
+Last compatibility check: 3.3
+
+## Build it yourself
+
+- `.\build.ps1 build` to create res.pak
+- `.\build.ps1 pub` to prepare publish directory to upload fork to steam workshop (change name in metadata.json if you want to do this)
+- `.\build.ps1 check` to unpak res.pak to `check` to check what files it contains
+- `.\build.ps1 ref` to unpak res.pak of main game in `reference` directory to veiw what's originally inside (it unpacks CDB and TMX files too)
+- `.\build.ps1 clean` to remove all files created by this script, including `publish/` and `res.pak`
+- `.\build.ps1 live` to patch YOUR ORIGINAL res.pak with this mod (it will allow to use this mod and still have achievements). WARNING: before running any other command besides clean, restore your original res.pak in game folder (Steam can do it for you)
---
+++ build.ps1
@@ -0,0 +1,75 @@
+param(
+    [string] $Target = "build"
+)
+
+$InstallDir = "C:\Program Files (x86)\Steam\steamapps\common\Dead Cells"
+
+function UnpackAll {
+    param(
+        [Parameter(Mandatory)][string] $Destination
+    )
+
+    PAKTool.exe -Expand -OutDir "$(pwd)\$Destination\unpacked" -RefPak "$InstallDir\res.pak"
+    CDBTool.exe -Expand -OutDir "$(pwd)\$Destination\castledb" -RefCdb "$(pwd)\$Destination\unpacked\data.cdb"
+    TmxTool.exe -Expand -TmxBin "$(pwd)\$Destination\unpacked\tiled" -TmxXml "$(pwd)\$Destination\tiled"
+}
+
+function PatchAll {
+    Copy-Item -Path "patches\183---Blowgun.json" -Destination "build\castledb\item\Ranged" -Force
+    Copy-Item -Path "patches\078---Blowgun.json" -Destination "build\castledb\weapon" -Force
+    Copy-Item -Path "patches\PrisonTube2.tmx" -Destination "build\tiled\tmx\Prison" -Force
+    Copy-Item -Path "patches\PrisonTube2NoSkel.tmx" -Destination "build\tiled\tmx\Prison" -Force
+
+    TmxTool.exe -Collapse -TmxBin "$(pwd)\build\tiled-bin" -TmxXml "$(pwd)\build\tiled"
+    Copy-Item -Path "build\tiled-bin\tmx\Prison\PrisonTube2.tmx" -Destination "build\unpacked\tiled\tmx\Prison" -Force
+    Copy-Item -Path "build\tiled-bin\tmx\Prison\PrisonTube2NoSkel.tmx" -Destination "build\unpacked\tiled\tmx\Prison" -Force
+    CDBTool.exe -Collapse -Indir "$(pwd)\build\castledb" -OutCdb "$(pwd)\build\unpacked\data.cdb"
+}
+
+function CreateModRes {
+    PAKTool.exe -CreateDiffPak -RefPak "$InstallDir\res.pak" -InDir "$(pwd)\build\unpacked" -OutPak "$(pwd)\res.pak"
+}
+
+function CheckModContents {
+    PAKTool.exe -Expand -OutDir "$(pwd)\check" -RefPak "$(pwd)\res.pak"
+}
+
+function LivePatch {
+    PAKTool.exe -Collapse -Indir "$(pwd)\build\unpacked" -OutPak "$InstallDir\res.pak"
+}
+
+function PrePublish {
+    New-Item -ItemType Directory -Path publish -Force >$null
+    Copy-Item -Path "metadata\settings.json" -Destination "publish" -Force
+    Copy-Item -Path "metadata\preview.jpg" -Destination "publish" -Force
+    Copy-Item -Path "res.pak" -Destination "publish" -Force
+}
+
+switch($Target) {
+    "build" {
+        UnpackAll -Destination build
+        PatchAll
+        CreateModRes
+    }
+    "check" {
+        CheckModContents
+    }
+    "ref" {
+        UnpackAll -Destination reference
+    }
+    "live" {
+        UnpackAll -Destination build
+        PatchAll
+        LivePatch
+    }
+    "pub" {
+        PrePublish
+    }
+    "clean" {
+        if (Test-Path "build") { Remove-Item -Path "build" -Force -Recurse }
+        if (Test-Path "check") { Remove-Item -Path "check" -Force -Recurse }
+        if (Test-Path "publish") { Remove-Item -Path "publish" -Force -Recurse }
+        if (Test-Path "reference") { Remove-Item -Path "reference" -Force -Recurse }
+        if (Test-Path "res.pak") { Remove-Item -Path "res.pak" -Force }
+    }
+}
